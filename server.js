2fe8aeda89d96239b9eebd1d566bea20c3b56226
Ashley Sands <ashley.sands01@gmail.com>
2026-01-28 08:43:17 -0800

    add port variable

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const logger = require('morgan');
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const logger = require('morgan')
 
-const testJwtRouter = require('./controllers/test-jwt');
-const authRouter = require('./controllers/auth');
-const userRouter = require('./controllers/users');
+const testJwtRouter = require('./controllers/test-jwt')
+const authRouter = require('./controllers/auth')
+const userRouter = require('./controllers/users')
 const hootsRouter = require('./controllers/hoots.js')
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
+PORT = process.env.PORT
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
 
-app.use(cors());
-app.use(express.json());
-app.use(logger('dev'));
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
 
-app.use('/auth', authRouter);
-app.use('/users', userRouter);
-app.use('/test-jwt', testJwtRouter);
+app.use('/auth', authRouter)
+app.use('/users', userRouter)
+app.use('/test-jwt', testJwtRouter)
 app.use('/hoots', hootsRouter)
 
-app.listen(3000, () => {
-  console.log('The express app is ready!');
-});
+app.listen(PORT, () => {
+    console.log('The express app is ready!')
+})
